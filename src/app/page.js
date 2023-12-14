f4d0f7daf1fe977ffde39ec07982e1c8702305f6
Erick <erick.damian@delmartg.com>
2023-12-13 22:44:12 -0500

    form login

--- src/app/page.js
+++ src/app/page.js
@@ -9,18 +9,18 @@ export default function Home(){
 
 
     return(
-        <div className="flex flex-wrap min-h-screen w-full content-center justify-center bg-gray-200 py-10">
-            <div className="w-2/4 h-5/6 flex justify-center border-2 rounded-md border-black">
+        <div className="flex flex-wrap min-h-screen w-full content-center justify-center py-10">
+            <div className="w-2/4 h-5/6 flex justify-center border-2 rounded-md border-black bg-gray-200 ">
                 <div className="w-1/2 flex flex-col p-12 space-y-4">
                     <h1 className="text-xl font-semibold pb-8">Sistema Simplificado de Planificacion y Gestion de la investigacion</h1>
                     <a className="text-gray-400">Por favor ingrese sus credenciales</a>
                     <form className="w-full space-y-4 flex flex-col justify-center pb-8">
                         <TextField
-                            label="Nombres"
+                            label="Usuario"
                             id="outlined-size-small"
                         />
                         <TextField
-                            label="Nombres"
+                            label="Contrasenha"
                             id="outlined-size-small"
                         />
                     </form>
