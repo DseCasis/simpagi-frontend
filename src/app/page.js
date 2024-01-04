6c3f8275f8c2b7f972f0ba9685d38b1d6fc00828
DseCasis <dse.casis@yavirac.edu.ec>
2024-01-04 15:06:54 -0500

    pruebas

--- src/app/page.js
+++ src/app/page.js
@@ -5,33 +5,15 @@ import TextField from "@mui/material/TextField";
 import React from "react";
 import Button from "@mui/material/Button";
 import Menu from "@/app/layout/Menu";
+import RegisterForm from "./views/RegisterForm";
 
 export default function Home(){
 
 
     return(
         <div>
-            <Menu></Menu>
-            <div className="flex justify-center items-center mt-20">
-                <div className="grid grid-cols-3 gap-12">
-                    <div className="w-52 text-center">
-                        <img src="/user-group.png" alt=""/>
-                        <p className="">INVESTIGADOR</p>
-                    </div>
-                    <div className="w-52 text-center">
-                        <img src="/user-group.png" alt=""/>
-                        <p className="">DIRECTOR</p>
-                    </div>
-                    <div className="w-52 text-center">
-                        <img src="/user-group.png" alt=""/>
-                        <p className="">RESPONSABLE</p>
-                    </div>
-                    <div className="w-52 text-center">
-                        <img src="/user-group.png" alt=""/>
-                        <p className="">DIRECTOR CENTRAL</p>
-                    </div>
-                </div>
-            </div>
+            <RegisterForm></RegisterForm>
+
         </div>
     )
 }
