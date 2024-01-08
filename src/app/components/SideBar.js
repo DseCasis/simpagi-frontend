87e4683690fa8fadfe006490148908d98ef8f8f2
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-01-08 14:19:21 -0500

    Actualizacion

---
+++ src/app/components/SideBar.js
@@ -0,0 +1,47 @@
+import React from "react";
+import Button from '@mui/material/Button';
+
+export default function SideBar() {
+    return (
+<div className="flex min-h-screen">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" />
+
+  {/* Barra lateral */}
+  <aside className="bg-green-800 text-white w-1/3 md:w-1/4 lg:w-1/5 flex flex-col items-center">
+    {/* Contenido de la barra lateral */}
+    <div className="p-4">
+      <img
+        src="https://img.freepik.com/vector-premium/icono-circulo-usuario-anonimo-ilustracion-vector-estilo-plano-sombra_520826-1931.jpg?w=740"
+        alt="Foto de usuario"
+        className="rounded-full h-32 w-32 lg:h-64 lg:w-64 object-cover"
+      />
+    </div>
+    <div className="p-3 flex flex-col items-center">
+      <p className="text-center text-white font-bold md:text-lg lg:text-xl">Juanito Andres Perez Ponce</p>
+      <p className="text-center text-white md:text-sm lg:text-base">alexjeremy10@hotmail.com</p>
+    </div>
+
+    <div>
+      <ul className="p-4 text-center text-white md:text-sm lg:text-base">
+        <li className="py-2 mb-6 font-bold"><a href="#">Enlace 1</a></li>
+        <li className="py-2 mb-6 font-bold"><a href="#">Enlace 2</a></li>
+        <li className="py-2 mb-6 font-bold"><a href="#">Enlace 3</a></li>
+        <li className="py-2 mb-6 font-bold"><a href="#">Enlace 4</a></li>
+      </ul>
+    </div>
+
+    <div className="flex-grow"></div>
+
+    <div className="w-full">
+      <Button className='bg-red-500 w-full py-6 md:py-8 lg:py-10 px-10 md:px-16 lg:px-20 rounded text-white' variant="contained" color="error">
+        <a href="#">
+          <i className="fas fa-door-open mr-2"></i> Cerrar Sesión
+        </a>
+      </Button>
+    </div>
+  </aside>
+  <script src="ruta/al/archivo.js"></script>
+</div>
+
+    )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,35 +1,17 @@
 "use client"
 
 import React from "react";
-import Button from "@mui/material/Button";
-import Menu from "@/app/layout/Menu";
+
+import LowerBar from "./components/LowerBar";
+import SideBar from "./components/SideBar";
 
 export default function Home(){
 
 
     return(
         <div>
-            <Menu></Menu>
-            <div className="flex justify-center items-center mt-20">
-                <div className="grid grid-cols-3 gap-12">
-                    <div className="w-52 text-center">
-                        <img src="/user-group.png" alt=""/>
-                        <p className="">INVESTIGADOR</p>
-                    </div>
-                    <div className="w-52 text-center">
-                        <img src="/user-group.png" alt=""/>
-                        <p className="">DIRECTOR</p>
-                    </div>
-                    <div className="w-52 text-center">
-                        <img src="/user-group.png" alt=""/>
-                        <p className="">RESPONSABLE</p>
-                    </div>
-                    <div className="w-52 text-center">
-                        <img src="/user-group.png" alt=""/>
-                        <p className="">DIRECTOR CENTRAL</p>
-                    </div>
-                </div>
-            </div>
+            <SideBar></SideBar>
+            <LowerBar></LowerBar>
         </div>
     )
 }
