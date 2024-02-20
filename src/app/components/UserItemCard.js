1c79808589b010635d2953c8fb4ee04404dac275
Jeremy339 <jal.pacho@yavirac.edu.ec>
2024-02-19 20:36:49 -0500

    UserList ordenado

--- src/app/components/UserItemCard.js
+++ src/app/components/UserItemCard.js
@@ -1,16 +1,16 @@
 const UserItemCard = ({ name, dni, email, hiteDate }) => {
     return (
         <ul className="grid grid-cols-4 gap-4 mt-6 text-gray-600 dark:text-gray-400">
-            <li className="col-span-1 flex flex-col capitalize">
+            <li className="col-span-1 flex flex-col capitalize text-left">
                 <span className="mt-2 text-black dark:text-gray-200">{dni}</span>
             </li>
-            <li className="col-span-1 flex flex-col capitalize">
+            <li className="col-span-1 flex flex-col capitalize text-left">
                 <span className="mt-2 text-black dark:text-gray-200">{name}</span>
             </li>
-            <li className="col-span-1 flex flex-col capitalize">
+            <li className="col-span-1 flex flex-col capitalize text-left">
                 <span className="mt-2 text-black dark:text-gray-200">{email}</span>
             </li>
-            <li className="col-span-1 flex flex-col capitalize">
+            <li className="col-span-1 flex flex-col capitalize text-left">
                 <span className="mt-2 text-black dark:text-gray-200">{hiteDate}</span>
             </li>
         </ul>
